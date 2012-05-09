287c0c8f3f70b1f95090ae7a95d7c7901e127c48
Caio Romão <caioromao@gmail.com>
2012-05-09 16:46:06 +0200

    Version bump to 1.05

--- lib/Data/FlexSerializer.pm
+++ lib/Data/FlexSerializer.pm
@@ -7,7 +7,7 @@ use MooseX::Types -declare => [ qw(
 ) ];
 use autodie;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 # Get the DEBUG constant from $Data::FlexSerializer::DEBUG or
 # $ENV{DATA_FLEXSERIALIZER_DEBUG}
